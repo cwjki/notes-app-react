e68d79d16f715508d659c07321e2c00f08333e3c
cwjki <cwjki97@gmail.com>
2022-02-09 19:06:23 -0500

    CRUD working

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -18,9 +18,21 @@ const NotePage = () => {
     }, [noteId])
 
     let getNode = async () => {
+        if (noteId ==='new') return
+
         let response = await fetch(`http://localhost:8000/notes/${noteId}`)
         let data = await response.json()
         setNote(data)
+    }
+
+    let createNote = async () => {
+        await fetch (`http://localhost:8000/notes/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({...note, 'updated': new Date() })
+        })
     }
 
     let updateNote = async () => {
@@ -45,7 +57,15 @@ const NotePage = () => {
     }
 
     let handleSubmit = () => {
-        updateNote()
+
+        if (noteId !== 'new' && !note.body){
+            deleteNote()
+        } else if (noteId !== 'new'){
+            updateNote()
+        } else if (noteId ==='new' && note !== null){
+            createNote()
+        }
+
         navigate('/')
     }
 
@@ -57,7 +77,13 @@ const NotePage = () => {
                         <ArrowLeft onClick={handleSubmit} />
                     </Link>
                 </h3>
-                <button onClick={deleteNote}>Delete</button>
+
+                { noteId !== 'new' ? (
+                    <button onClick={deleteNote}>Delete</button>
+                ): (
+                    <button onClick={handleSubmit}>Done</button>
+                )}
+                
             </div>
             
             <textarea onChange={(e) => (setNote({...note, 'body':e.target.value}))} value={note?.body}></textarea>
